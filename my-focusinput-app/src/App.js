54677e5b07d2852b0bef9a11c08f6c7d8d649290
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-12-14 10:51:41 +0100

    Created focus input

--- my-focusinput-app/src/App.js
+++ my-focusinput-app/src/App.js
@@ -6,17 +6,26 @@ class App extends Component {
     this.state = {
       items: ['Item 1', 'Item 2', 'Item 3'], 
     };
+    this.inputRef = React.createRef(); 
   }
+
+  focusInput = () => {
+    this.inputRef.current.focus(); 
+  };
 
   render() {
     return (
       <div>
         <h1>Список елементів</h1>
         <ul>
-          {this.state.items.map((item, index) => (
+        {this.state.items.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
+        <div>
+          <input ref={this.inputRef} type="text" placeholder="Введіть текст" />
+          <button onClick={this.focusInput}>Focus input</button>
+        </div>
       </div>
     );
   }
